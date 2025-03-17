ed04388ac20411cabf4da2e09e88f98819b533d6
burakberat <burakorucoglu@outlook.com>
2025-03-17 14:30:00 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://laaknfjq:lHyWIu6ZfyhuDjcEFI4CWZl0VjDXcrrg@seal.lmq.cloudamqp.com/laaknfjq");
+
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"Message {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                Console.WriteLine($"Mesaj gönderilmiştir: {message}");
+            });
+
+
+            Console.ReadLine();
+        }
+    }
+}
