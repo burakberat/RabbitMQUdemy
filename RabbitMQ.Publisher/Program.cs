abca2b42b163e3635dff76092f5a09af19ea12c3
burakberat <burakorucoglu@outlook.com>
2025-03-19 16:29:29 +0300

    Exchange Types

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,5 +1,7 @@
 ﻿using RabbitMQ.Client;
+using Shared;
 using System.Text;
+using System.Text.Json;
 
 namespace RabbitMQ.Publisher
 {
@@ -14,34 +16,85 @@ namespace RabbitMQ.Publisher
     {
         static void Main(string[] args)
         {
+            #region Ortak
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqps://laaknfjq:lHyWIu6ZfyhuDjcEFI4CWZl0VjDXcrrg@seal.lmq.cloudamqp.com/laaknfjq");
-
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
+            #endregion
 
-            //channel.ExchangeDeclare("logs-fanout", durable:true, type:ExchangeType.Fanout);
-            // channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
-            channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
 
-            Random rnd = new Random();
+            #region Fanout Exchange
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
-                LogNames log1 = (LogNames)rnd.Next(1, 5);
-                LogNames log2 = (LogNames)rnd.Next(1, 5);
-                LogNames log3 = (LogNames)rnd.Next(1, 5);
-
-                var routeKey = $"{log1}.{log2}.{log3}";
-                string message = $"log-type {log1}-{log2}-{log3}";
-                var messsageBody = Encoding.UTF8.GetBytes(message);
-
-                channel.BasicPublish("logs-topic", routeKey, null, messsageBody);
-
-                Console.WriteLine($"Log gönderilmiştir: {message}");
+                string message = $"log {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("logs-fanout", "", null, messageBody);
+                Console.WriteLine($"Mesaj gönderilmiştir : {message}");
             });
-
-
             Console.ReadLine();
+            #endregion
+
+            #region Topic Exchange
+            //channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+            //Random rnd = new Random();
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    LogNames log1 = (LogNames)rnd.Next(1, 5);
+            //    LogNames log2 = (LogNames)rnd.Next(1, 5);
+            //    LogNames log3 = (LogNames)rnd.Next(1, 5);
+
+            //    var routeKey = $"{log1}.{log2}.{log3}";
+            //    string message = $"log-type: {log1}-{log2}-{log3}";
+            //    var messageBody = Encoding.UTF8.GetBytes(message);
+            //    channel.BasicPublish("logs-topic", routeKey, null, messageBody);
+            //    Console.WriteLine($"Log gönderilmiştir : {message}");
+            //});
+            //Console.ReadLine(); 
+            #endregion
+
+            #region Direct Exchange
+            //channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+            //Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            //{
+            //    var routeKey = $"route-{x}";
+            //    var queueName = $"direct-queue-{x}";
+            //    channel.QueueDeclare(queueName, true, false, false);
+            //    channel.QueueBind(queueName, "logs-direct", routeKey, null);
+            //});
+
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    LogNames log = (LogNames)new Random().Next(1, 5);
+            //    string message = $"log-type: {log}";
+            //    var messageBody = Encoding.UTF8.GetBytes(message);
+            //    var routeKey = $"route-{log}";
+            //    channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+            //    Console.WriteLine($"Log gönderilmiştir : {message}");
+            //});
+            //Console.ReadLine(); 
+            #endregion
+
+            #region HeaderExchange ve Text Message
+            //channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+            //Dictionary<string, object> headers = new Dictionary<string, object>();
+            //headers.Add("format", "pdf");
+            //headers.Add("shape2", "a4");
+
+            //var properties = channel.CreateBasicProperties();
+            //properties.Headers = headers;
+            //properties.Persistent = true;
+
+            //var product = new Product { Id = 1, Name = "Kalem", Price = 100, Stock = 10 };
+            //var productJsonString = JsonSerializer.Serialize(product);
+
+            //channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
+            //Console.WriteLine("mesaj gönderilmiştir");
+            //Console.ReadLine(); 
+            #endregion
         }
     }
 }
--- RabbitMQ.Subscriber/Program.cs
+++ RabbitMQ.Subscriber/Program.cs
@@ -1,6 +1,8 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using Shared;
 using System.Text;
+using System.Text.Json;
 
 namespace RabbitMQ.Subscriber
 {
@@ -8,36 +10,95 @@ namespace RabbitMQ.Subscriber
     {
         static void Main(string[] args)
         {
+            #region ortak
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqps://laaknfjq:lHyWIu6ZfyhuDjcEFI4CWZl0VjDXcrrg@seal.lmq.cloudamqp.com/laaknfjq");
-
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
+            #endregion
 
+            #region Fanout Exchange
+            var randomQueueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(randomQueueName, "logs-fanout", "", null);
             channel.BasicQos(0, 1, false);
-
             var consumer = new EventingBasicConsumer(channel);
-            var queueName = channel.QueueDeclare().QueueName;
-            var routeKey = "*.Error.*";
-            channel.QueueBind(queueName, "logs-topic", routeKey);
-            channel.BasicConsume(queueName, false, consumer);
-
-            Console.WriteLine("Loglar Dinleniyor.");
+            channel.BasicConsume(randomQueueName, false, consumer);
+            Console.WriteLine("Logları dinleniyor...");
 
             consumer.Received += (object sender, BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
                 Thread.Sleep(1500);
                 Console.WriteLine("Gelen Mesaj:" + message);
-                //File.AppendAllText("logs-critical.txt", message + "\n");
-
                 channel.BasicAck(e.DeliveryTag, false);
             };
+            Console.ReadLine(); 
+            #endregion
 
-            Console.ReadLine();
+            #region Topic Exchange
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+            //var queueName = channel.QueueDeclare().QueueName;
+            //var routekey = "Info.#";
+            //channel.QueueBind(queueName, "logs-topic", routekey);
+            //channel.BasicConsume(queueName, false, consumer);
 
-            //deneme TopixExchange
-            //deneme commit
+            //Console.WriteLine("Logları dinleniyor...");
+
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //    Thread.Sleep(1500);
+            //    Console.WriteLine("Gelen Mesaj:" + message);
+            //    // File.AppendAllText("log-critical.txt", message+ "\n");
+            //    channel.BasicAck(e.DeliveryTag, false);
+            //};
+            //Console.ReadLine(); 
+            #endregion
+
+            #region Direct Exchange
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+            //var queueName = "direct-queue-Critical";
+            //channel.BasicConsume(queueName, false, consumer);
+            //Console.WriteLine("Logları dinleniyor...");
+
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //    Thread.Sleep(1500);
+            //    Console.WriteLine("Gelen Mesaj:" + message);
+            //    // File.AppendAllText("log-critical.txt", message+ "\n");
+            //    channel.BasicAck(e.DeliveryTag, false);
+            //};
+            //Console.ReadLine(); 
+            #endregion
+
+            #region HeaderExchange ve Text Message
+            //channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+            //var queueName = channel.QueueDeclare().QueueName;
+
+            //Dictionary<string, object> headers = new Dictionary<string, object>();
+
+            //headers.Add("format", "pdf");
+            //headers.Add("shape", "a4");
+            //headers.Add("x-match", "any");
+            //channel.QueueBind(queueName, "header-exchange", String.Empty, headers);
+            //channel.BasicConsume(queueName, false, consumer);
+            //Console.WriteLine("Logları dinleniyor...");
+
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //    Product product = JsonSerializer.Deserialize<Product>(message);
+            //    Thread.Sleep(1500);
+            //    Console.WriteLine($"Gelen Mesaj: {product.Id}-{product.Name}-{product.Price}-{product.Stock}");
+            //    channel.BasicAck(e.DeliveryTag, false);
+            //};
+            //Console.ReadLine();
+            #endregion
         }
     }
 }
